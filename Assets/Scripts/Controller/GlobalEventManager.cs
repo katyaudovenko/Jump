e069fc547708302def5c39261ce817edd82e5e6d
katyaudovenko <katya.udovenko.1996@gmail.com>
2022-07-28 17:07:09 +0400

    Created GlobalEventManager

---
+++ Assets/Scripts/Controller/GlobalEventManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Controller
+{
+    public static class GlobalEventManager
+    {
+        public static event Action StartGame;
+        public static void OnStartGame() => StartGame?.Invoke();
+
+        public static event Action EndGame;
+        public static void OnEndGame() => EndGame?.Invoke();
+    }
+}
