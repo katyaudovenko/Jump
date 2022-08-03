c95e03f33f7706f55230c62f31148e839cc2117f
katyaudovenko <katya.udovenko.1996@gmail.com>
2022-08-03 15:07:04 +0400

    Created jump for player

---
+++ Assets/Scripts/Model/PlayerComponents/PlayerJump.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Model.PlayerComponents
+{
+    public class PlayerJump : MonoBehaviour
+    {
+        private const float GravityScale = 0.01f;
+        
+        [SerializeField] private float jumpForce;
+
+        public bool IsProp { get; private set; }
+        private Rigidbody2D _rigidbody2D;
+
+        private void Awake() => 
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        
+        public void Jump()
+        {
+            IsProp = false;
+            _rigidbody2D.velocity = (transform.right + Vector3.up).normalized * jumpForce;
+        }
+
+        public void DoubleJump()
+        {
+            Jump();
+            transform.Rotate(0,180,0);
+        }
+
+        public void StopJump()
+        {
+            IsProp = true;
+            _rigidbody2D.velocity = Vector2.zero;
+            _rigidbody2D.gravityScale = GravityScale;
+        }
+    }
+}
---
+++ Assets/Scripts/Model/PlayerComponents/PlayerMovementController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Model.PlayerComponents
+{
+    public class PlayerMovementController : MonoBehaviour
+    {
+        private const string SlidingZoneTag = "SlidingZone";
+        
+        private PlayerJump _jumpComponent;
+
+        private void Awake() => 
+            _jumpComponent = GetComponent<PlayerJump>();
+
+        private void Update()
+        {
+            if (!Input.GetMouseButtonDown(0)) 
+                return;
+            
+            _jumpComponent.Jump();
+            
+            if (!_jumpComponent.IsProp) 
+                _jumpComponent.DoubleJump();
+
+        }
+        
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.gameObject.tag.Equals(SlidingZoneTag)) 
+                _jumpComponent.StopJump();
+        }
+
+    }
+}
