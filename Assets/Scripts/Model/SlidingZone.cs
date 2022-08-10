02155af747d600ce4f2ba2c44434fbd5740357cf
katyaudovenko <katya.udovenko.1996@gmail.com>
2022-08-10 18:50:33 +0400

    Create sliding zone component

---
+++ Assets/Scripts/Model/SlidingZone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Model.PlayerComponents;
+using UnityEngine;
+
+namespace Model
+{
+    public class SlidingZone : MonoBehaviour
+    {
+        private const string PlayerTag = "Player";
+
+        private PlayerJump _jumpComponent;
+        
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag.Equals(PlayerTag))
+            {
+                _jumpComponent = collision.gameObject.GetComponent<PlayerJump>();
+                _jumpComponent.StopJump();
+            }
+        }
+    }
+}
