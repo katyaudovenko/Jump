e689ee8e86c54c0408d3318b1171f77d2c3499d1
katyaudovenko <katya.udovenko.1996@gmail.com>
2022-09-08 15:30:21 +0400

    Created spawner for player

---
+++ Assets/Scripts/Modules/Spawn/GameEntitiesSpawn/PlayerSpawner.cs
@@ -0,0 +1,33 @@
+﻿using Infrastructure.Services;
+using Libs.Components;
+using Modules.Events;
+using Modules.Game;
+using UnityEngine;
+
+namespace Modules.Spawn.GameEntitiesSpawn
+{
+    public class PlayerSpawner : MonoBehaviour
+    {
+        [SerializeField] private CameraMove cameraMove;
+        
+        private GameFactory _gameFactory;
+
+        private void Awake()
+        {
+            _gameFactory = ServiceLocator.Instance.GetService<GameFactory>();
+            SpawnPlayer();
+        }
+
+        private void OnEnable() => 
+            GlobalEventManager.StartGame += SpawnPlayer;
+
+        private void OnDisable() => 
+            GlobalEventManager.StartGame -= SpawnPlayer;
+
+        private void SpawnPlayer()
+        {
+            var playerTransform = _gameFactory.CreatePlayer(transform, Vector3.zero);
+            cameraMove.SetPlayerTransform(playerTransform);
+        }
+    }
+}
