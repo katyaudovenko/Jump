e0848a5b8e1b14a12e8e2fc9582dd2e8e195f926
katyaudovenko <katya.udovenko.1996@gmail.com>
2022-09-08 14:06:15 +0400

    Added new unity files

---
+++ Assets/Scripts/Libs/Extensions/RandomExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Random = UnityEngine.Random;
+
+namespace Libs.Extensions
+{
+    public static class RandomExtension
+    {
+        public static T GetRandomItem<T>(this IEnumerable<T> list, Func<T, float> executor)
+        {
+            var sum = list.Sum(executor);
+            var randomPoint = Random.value * sum;
+            foreach (var item in list)
+            {
+                var chance = executor(item);
+                if (chance > randomPoint)
+                    return item;
+                
+                randomPoint -= chance;
+            }
+
+            return list.Last();
+        }
+    }
+}
