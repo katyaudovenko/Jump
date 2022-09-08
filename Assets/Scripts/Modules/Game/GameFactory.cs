6480a44d00fa8941e38b7a5f5839d918df2be169
katyaudovenko <katya.udovenko.1996@gmail.com>
2022-09-08 14:06:57 +0400

    Created game factory

---
+++ Assets/Scripts/Modules/Game/GameFactory.cs
@@ -0,0 +1,30 @@
+﻿using Infrastructure.Pool.ByType;
+using Infrastructure.Services;
+using Modules.PlayerComponents;
+using UnityEngine;
+
+namespace Modules.Game
+{
+    public class GameFactory : IService
+    {
+        private const string PoolContainerPath = "Prefabs/GameEntitiesPoolContainer";
+        
+        private TypePoolContainer _poolContainer;
+
+        public void Initialize()
+        {
+            var prefab = Resources.Load<TypePoolContainer>(PoolContainerPath);
+            _poolContainer = Object.Instantiate(prefab);
+            _poolContainer.CreatePools();
+        }
+
+        public PlayerCommonComponent CreatePlayer(Transform transform, Vector3 position)
+        {
+            var player = _poolContainer.GetFreeElement<PlayerCommonComponent>();
+            player.transform.position = position;
+            player.transform.SetParent(transform);
+            player.SetupPool(_poolContainer);
+            return player;
+        }
+    }
+}
