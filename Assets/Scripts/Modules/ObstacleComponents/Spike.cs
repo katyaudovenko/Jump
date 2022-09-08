7b77cffe798c4cad47a674dc34b2e642b9a011f1
katyaudovenko <katya.udovenko.1996@gmail.com>
2022-09-08 15:29:42 +0400

    Logic for destroy player has been created

---
+++ Assets/Scripts/Modules/ObstacleComponents/Spike.cs
@@ -0,0 +1,34 @@
+﻿using Libs.Components;
+using Modules.PlayerComponents;
+using UnityEngine;
+
+namespace Modules.ObstacleComponents
+{
+    public class Spike : MonoBehaviour
+    {
+        private CollisionComponent _collisionComponent;
+
+        private Collision2D _playerCollision;
+        private PlayerDestroyComponent _playerDestroyComponent;
+        
+        private void Awake() => 
+            _collisionComponent = GetComponent<CollisionComponent>();
+
+        private void OnEnable() => 
+            _collisionComponent.OnCollisionEnter += OnCollision;
+
+        private void OnDisable() => 
+            _collisionComponent.OnCollisionEnter -= OnCollision;
+
+        private void OnCollision(Collision2D collision)
+        {
+            if (_playerCollision != collision)
+            {
+                _playerDestroyComponent = collision.gameObject.GetComponent<PlayerDestroyComponent>();
+                _playerCollision = collision;
+            }
+            
+            _playerDestroyComponent.DestroyPlayer();
+        }
+    }
+}
---
+++ Assets/Scripts/Modules/PlayerComponents/PlayerDestroyComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Infrastructure.Pool;
+using Infrastructure.Pool.ByType;
+using UnityEngine;
+
+namespace Modules.PlayerComponents
+{
+    public class PlayerDestroyComponent : MonoBehaviour, IPoolBehaviour
+    {
+        public event Action OnDestroyPlayer;
+        private TypePoolContainer _poolContainer;
+        public void SetupPool(TypePoolContainer poolContainer) => 
+            _poolContainer = poolContainer;
+
+        public void DestroyPlayer()
+        {
+            _poolContainer.ReturnElement(this);
+            OnDestroyPlayer?.Invoke();
+        }
+
+        public void OnInitialize() { }
+        public void OnSetup() { }
+        public void OnReset() { }
+    }
+}
